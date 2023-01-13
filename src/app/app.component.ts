fb092d50c8d76fcd8f900bed2f92ac5b3bb94664
mano90 <antsomanoirina90gmail.com>
2023-01-13 21:11:59 +0300

    show pie chart with legend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import { Data } from './Interfaces/data';
 import { AppService } from './services/app.service';
 Chart.register(...registerables);
 @Component({
@@ -7,48 +8,79 @@ Chart.register(...registerables);
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'front_end';
+export class AppComponent implements OnInit, OnDestroy {
+  COLORS: string[] = [
+    '#0081C9',
+    '#FFC93C',
+    '#820000',
+    '#2B3A55',
+    '#FF7B54',
+    '#7F669D',
+    '#3A8891',
+    '#9E7676',
+    '#3C2317',
+    '#395144',
+    '#182747',
+  ];
+  data: Data[] = [];
+  typeChart: any = 'pie';
+  chart: any;
   constructor(private service: AppService) {}
   ngOnInit(): void {
-    this.service.listeLicense().subscribe((res) => {
-      console.log(res);
-    });
-    var myChart = new Chart('myChart', {
-      type: 'bar',
+    this.chart = new Chart('MyChart', {
+      type: this.typeChart,
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [
           {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
+            label: 'License',
+            data: [],
           },
         ],
       },
       options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
+        responsive: true,
+        aspectRatio: 2.5,
       },
     });
+    this.listeLicense();
   }
+
+  listeLicense(quantite?: number) {
+    this.service.listeLicense(quantite).subscribe(
+      (res) => {
+        this.updateData(res);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  listeLanguage(quantite?: number) {
+    this.service.listeLanguage(quantite).subscribe(
+      (res) => {
+        this.updateData(res);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private updateData(data: Data[]) {
+    this.chart.data.labels.length = 0;
+    this.chart.data.datasets.forEach((dataset: any) => {
+      dataset.data.length = 0;
+    });
+    this.chart.data.labels.push(...data.map((e) => e.label));
+
+    this.chart.data.datasets.forEach((dataset: any) => {
+      dataset.backgroundColor = this.COLORS;
+      dataset.data.push(...data.map((e) => e.quantity));
+    });
+    this.chart.update();
+  }
+
+  ngOnDestroy(): void {}
 }
